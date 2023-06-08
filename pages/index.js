a315df776ded09f491c5e4a66fd66400d4c71af8
Reidpan1 <129805612+Reidpan1@users.noreply.github.com>
2023-06-08 07:44:37 -0500

    init commit

---
+++ pages/index.js
@@ -0,0 +1,15 @@
+import { Fragment } from 'react';
+
+function HomePage() {
+  return (
+    <Fragment>
+      <Hero />
+      <FeaturedPosts />
+    </Fragment>
+  );
+}
+
+export default HomePage;
+
+// 1) Hero -> present ourselves
+// 2) Featured Posts
